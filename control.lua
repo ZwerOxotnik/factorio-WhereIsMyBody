181cf50c4a651449a365edbc3ed8007963e5ca70
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 23:52:32 +0300

    Update to 2.0.6

--- control.lua
+++ control.lua
@@ -1,1 +1,5 @@
-require("event_handler").add_lib(require("models/WhereIsMyBody"))
+if script.active_mods["zk-lib"] then
+	require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua").add_lib(require("models/WhereIsMyBody"))
+else
+	require("event_handler").add_lib(require("models/WhereIsMyBody"))
+end
