8d96c969f7a92e0f3de7fb41a9beb6201a5f716d
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-06-21 10:40:54 -0400

    Made Zip Script More Resiliant

--- .scripts/zip_mod.sh
+++ .scripts/zip_mod.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
+#!/bin/env bash
 ### Run this script after updating the mod to prepare a zip of it.
 
 
 ### REPOSITORY is current working directory
-REPOSITORY=`pwd`
-cd "$REPOSITORY/"
+
+current=$PWD
+repository="$(dirname -- $(readlink -fn -- "$0"; echo x))/../"
+
+cd $repository
 
 
 ### Get mod name and version from info.json
 ### https://stedolan.github.io/jq/
+
 mod_name=`cat info.json|jq -r .name`
 mod_ver=`cat info.json|jq -r .version`
 
 
 ### Prepare zip for Factorio native use and mod portal
+
 name="${mod_name}_$mod_ver"
+
 git clean -xdf
-cd ..
-7z a -xr'!.*' "$REPOSITORY/$name.zip" "$REPOSITORY"
+
+cd $current
+
+7z a -xr'!.*' "$repository/$name.zip" "$repository"
